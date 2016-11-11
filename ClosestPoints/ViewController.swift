0f848a077cad85c41ad2499c22149d9ec3851fc2
Robert Huston <robert.huston@pinpointdynamics.com>
2016-11-11 13:09:48 -0500

    Initial Commit

---
+++ ClosestPoints/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ClosestPoints
+//
+//  Created by Robert Huston on 11/11/16.
+//  Copyright © 2016 Pinpoint Dynamics. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
