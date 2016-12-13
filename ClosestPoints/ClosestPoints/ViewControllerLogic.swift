2198ecc1038f67f1cc77cde15e76010c7504b209
Robert Huston <robert.huston@pinpointdynamics.com>
2016-12-13 18:15:04 -0500

    Changed maximum number of points to 1000

--- ClosestPoints/ClosestPoints/ViewControllerLogic.swift
+++ ClosestPoints/ClosestPoints/ViewControllerLogic.swift
@@ -17,7 +17,7 @@ import Cocoa
 class ViewControllerLogic: NSObject {
 
     let minNumberOfPoints = 2
-    let maxNumberOfPoints = 100
+    let maxNumberOfPoints = 1000
 
     var pointCollection: PointCollection! = PointCollection()
     var definitionManager: DefinitionManager! = DefinitionManager()
