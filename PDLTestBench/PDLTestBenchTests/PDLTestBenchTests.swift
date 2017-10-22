a8f20e79c93c44091eb8847f5cc28080fc4fec23
Robert Huston <robert.huston@pinpointdynamics.com>
2017-10-22 11:18:09 -0400

    Ensure that Swift Language Version is set to 3.2

--- PDLTestBench/PDLTestBenchTests/PDLTestBenchTests.swift
+++ PDLTestBench/PDLTestBenchTests/PDLTestBenchTests.swift
@@ -10,21 +10,22 @@ import XCTest
 @testable import PDLTestBench
 
 class PDLTestBenchTests: XCTestCase {
-    
-//    override func setUp() {
-//        super.setUp()
-//        // Put setup code here. This method is called before the invocation of each test method in the class.
-//    }
 
-//    override func tearDown() {
-//        // Put teardown code here. This method is called after the invocation of each test method in the class.
-//        super.tearDown()
-//    }
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
 
-//    func testExample() {
-//        // This is an example of a functional test case.
-//        // Use XCTAssert and related functions to verify your tests produce the correct results.
-//    }
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(true)
+    }
 
 //    func testPerformanceExample() {
 //        // This is an example of a performance test case.
@@ -32,5 +33,5 @@ class PDLTestBenchTests: XCTestCase {
 //            // Put the code you want to measure the time of here.
 //        }
 //    }
-    
+
 }
